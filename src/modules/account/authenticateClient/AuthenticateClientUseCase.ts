01bb9718848da75228540a1163c3cd4efa1d09e0
Eduardo Luiz <hello@eduardoluiz.dev>
2022-09-17 14:15:46 -0300

    feat: client authenticate

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/prismaClient";
+
+interface IAuthenticateClientDTO {
+  username: string;
+  password: string;
+}
+
+class AuthenticateClientUseCase {
+  async execute({username, password}:IAuthenticateClientDTO) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username
+      }
+    })
+
+    if (!client) {
+      throw new Error('username or password invalid.')
+    }
+
+    const passwordMatch = await compare(password, client.password)
+
+    if (!passwordMatch) {
+      throw new Error('username or password invalid.')
+    }
+
+    const token = sign({username}, '6927c662b7af5a2f123dee892fa22a36', {
+      subject: client.id,
+      expiresIn: '1d'
+    })
+
+    return token
+  }
+}
+
+export { AuthenticateClientUseCase }
