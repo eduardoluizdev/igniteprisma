1b28a4e719c6abb41775a26f2bd06f60af525d2e
Eduardo Luiz <hello@eduardoluiz.dev>
2022-09-17 13:51:14 -0300

    feat: create client

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,35 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateClientDTO {
+  username: string;
+  password: string;
+}
+
+class CreateClientUseCase {
+
+  async execute({username, password}:ICreateClientDTO) {
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username
+      }
+    })
+
+    if (clientExist) {
+      throw new Error('Client already exists')
+    }
+
+    const hashPassword = await hash(password, 10)
+
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      }
+    })
+
+    return client
+  }
+} 
+
+export { CreateClientUseCase }
